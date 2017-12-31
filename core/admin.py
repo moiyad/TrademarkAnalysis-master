cbf3ea28f12c88ecba07ee96930f331bbaec388d
moiyad <moiyad117@gmail.com>
2017-12-31 14:32:07 +0300

    Trademark 3.2.3 -request Trademark registration

--- core/admin.py
+++ core/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Demo
 
 
-class DemoModelAdmin(admin.ModelAdmin):
-    button_field = ['image']
-
-    class Meta:
-        model = Demo
-
-
-admin.site.register(Demo, DemoModelAdmin)
+# class DemoModelAdmin(admin.ModelAdmin):
+#     button_field = ['image']
+#
+#     class Meta:
+#         model = Demo
+#
+#
+# admin.site.register(Demo, DemoModelAdmin)
